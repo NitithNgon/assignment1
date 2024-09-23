4d0c6d61b201c005bdf66b98cd187826e3f015bf
Peiam_eei-01_MSI <nitith88871@gmail.com>
2024-09-23 21:39:09 +0700

    commit

---
+++ Classify_peaks_bad_events.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.datasets import electrocardiogram
+from scipy.signal import find_peaks
+
+def classify_peaks_bad_events():
+
+
+    return
+
+
--- test.py
+++ test.py
@@ -2,53 +2,55 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# print("Current Working Directory:", os.getcwd())
 
+
+# print("current working directory:", os.getcwd())
 # loop read event
-
-# C:\Users\eei-0\Desktop\assignment1\to_Peiam\bad_events_dirtyAX\20240409_085745_4
-# C:\Users\eei-0\Desktop\assignment1\to_Peiam\bad_events_raining\20240912_023357_1
-# C:\Users\eei-0\Desktop\assignment1\to_Peiam\bad_events_raining\20240912_022716_2
-event_path = "to_Peiam/bad_events_dirtyAX/20240409_085745_4/event.txt"
+# c:\users\eei-0\desktop\assignment1\to_peiam\bad_events_dirty_ax\20240409_085745_4
+# c:\users\eei-0\desktop\assignment1\to_peiam\bad_events_raining\20240912_023357_1
+# c:\users\eei-0\desktop\assignment1\to_peiam\bad_events_raining\20240912_022716_2
+event_path = "to_peiam/bad_events_raining/20240912_022716_2/event.txt"
 file = open(event_path, 'r')
 
 # cut lines
 lines = file.readlines()
 usedline = lines[2:-1]
 
-usedData = []
-for i in usedline: usedData.append((i.strip()).split("\t"))
-signalArray = np.array(usedData,dtype=float)
-print(signalArray[:,-4:])
+used_data = []
+for i in usedline: used_data.append((i.strip()).split("\t"))
+signal_array = np.array(used_data,dtype=float)
 
-FULL_uV_Exact_Channel_list_dirtyAX = np.array([1, 2, 3, 4, 5, 6])
-FULL_uV_Exact_Channel_list = FULL_uV_Exact_Channel_list_dirtyAX
-# FULL_uV_Exact_Channel_list = [1, 2, 9, 13, 14, 21]
-FULL_uV = signalArray[:, FULL_uV_Exact_Channel_list_dirtyAX-1]
-# print(FULL_uV.shape, FULL_uV)
-Start_FULL_uV = np.full_like(FULL_uV, FULL_uV[0,:])
-# print(Start_FULL_uV.shape, Start_FULL_uV)
-Calibrate_FULL_uV = FULL_uV - Start_FULL_uV
 
-Axle_cm_direction = 0 # 0 or 1
-Axle_cm_Exact_Channel_list = [[-2,None],[-4,-2]]
-Axle_cm = signalArray[:, Axle_cm_Exact_Channel_list[Axle_cm_direction][0]:Axle_cm_Exact_Channel_list[Axle_cm_direction][1] ]
-print(Axle_cm.shape, Axle_cm)
-Min_Top_Flatline_Ax = min([ max(Axle_cm[:,i]) for i in range(2)]) # minimum top flat line of all AX
+# input channel
+full_u_v_exact_channel_list_dirty_ax = np.array([1, 2, 3, 4, 5, 6])       # bad_events_dirty_ax/20240409_085745_4
+full_u_v_exact_channel_list_raining = np.array([1, 2, 9, 13, 14, 21])    # bad_events_raining/20240912_022716_2
+full_u_v_exact_channel_list = full_u_v_exact_channel_list_raining
+axle_cm_direction = 1                                                   # 0 or 1
+
+# zero shift initial point
+full_u_v = signal_array[:, full_u_v_exact_channel_list-1 ]
+start_full_u_v = full_u_v[0,:]
+calibrate_full_u_v = full_u_v - start_full_u_v
+
+axle_cm_exact_channel_list = [[-2,-1],[-4,-3]]      # last 4 channels are ax
+axle_cm = signal_array[:, axle_cm_exact_channel_list[axle_cm_direction] ]
+min_top_flatline_ax = min(axle_cm.max(axis=0))      # minimum top flat line of all ax
+print(calibrate_full_u_v.shape, calibrate_full_u_v)
+print(axle_cm.shape, axle_cm)
 
 
 fig, axs = plt.subplots(2, 1, figsize=(8, 7), gridspec_kw={'height_ratios': [2, 1]})
 # fig.tight_layout(pad=3)
-axs[0].plot(Calibrate_FULL_uV)
-axs[0].set_title("Strain Event :2", fontweight="bold")
-axs[0].set_ylabel('FULL_uV')
+axs[0].plot(calibrate_full_u_v)
+axs[0].set_title("strain event :2", fontweight="bold")
+axs[0].set_ylabel('full_u_v')
 axs[0].set_xticklabels([])
-axs[0].legend(list(map(str, FULL_uV_Exact_Channel_list)),bbox_to_anchor=(1.01, 0.75), loc='upper left')
+axs[0].legend(list(map(str, full_u_v_exact_channel_list)),bbox_to_anchor=(1.01, 0.75), loc='upper left')
 
-axs[1].plot(Axle_cm)
-axs[1].set_ylim(0, Min_Top_Flatline_Ax )
-axs[1].set_yticks(np.arange(0, Min_Top_Flatline_Ax, step=250))
-axs[1].set_ylabel('Axle_cm')
+axs[1].plot(axle_cm)
+axs[1].set_ylim(0, min_top_flatline_ax )
+axs[1].set_yticks(np.arange(0, min_top_flatline_ax, step=250))
+axs[1].set_ylabel('axle_cm')
 axs[1].legend(list(map(str, [1,2])),bbox_to_anchor=(1.01, 0.75), loc='upper left')
 plt.show()
 
@@ -56,13 +58,16 @@ plt.show()
 
 fig, axs = plt.subplots(1, 2, figsize=(16, 4), gridspec_kw={'width_ratios': [1, 1]})
 fig.tight_layout(pad=1)
-Flip_Axle_cm = Min_Top_Flatline_Ax - Axle_cm
-topgraph_Flip_Axle_cm = max([ max(Flip_Axle_cm[:,i]) for i in range(2)])
-axs[0].plot(Flip_Axle_cm)
-axs[0].set_ylim(0, topgraph_Flip_Axle_cm )
-axs[0].set_yticks(np.arange(0, topgraph_Flip_Axle_cm, step=250))
+flip_axle_cm = min_top_flatline_ax - axle_cm
+topgraph_flip_axle_cm = max([ max(flip_axle_cm[:,i]) for i in range(2)])
+axs[0].plot(flip_axle_cm)
+axs[0].set_ylim(0, topgraph_flip_axle_cm )
+axs[0].set_yticks(np.arange(0, topgraph_flip_axle_cm, step=250))
+
+np.diag()
+time_range= [range(0, flip_axle_cm.shape[0])]
+axs[1].scatter(np.full_like(flip_axle_cm, np.transpose(time_range)),flip_axle_cm)
 
 
-Time_range= [range(0, Flip_Axle_cm.shape[0])]
-axs[1].scatter(np.full_like(Flip_Axle_cm, np.transpose(Time_range)),Flip_Axle_cm)
+
 plt.show()
